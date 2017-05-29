fb5577aaa7e35622614afcb080084bfa1164f7f8
David O'Neill <doneill612@gmail.com>
2017-05-29 22:10:45 +0200

    Rename main, randomized seed generation
    Added a random element to seed generation.

--- src/runner.py
+++ src/runner.py
@@ -1,9 +1,7 @@
 import utils
 import network
 
-import os
-# Suppress some Keras warnings ...
-os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+from random import randint
 
 NOTE_MEMORY = 30
 
@@ -15,16 +13,18 @@ max_t = utils.scale_note_data(notes)
 X = []
 y = []
 
+rand_start = randint(0, len(notes) - NOTE_MEMORY)
+
 for i in range(len(notes) - NOTE_MEMORY):
     X.append(notes[i:i + NOTE_MEMORY])
     y.append(notes[i + NOTE_MEMORY])
 
-seed = notes[0:NOTE_MEMORY]
+seed = notes[rand_start: rand_start + NOTE_MEMORY]
 
-model = network.build_trained_network(X, y, n_epochs=3600)
+model = network.load_model('savedmodels/model_3600_epochs_111.h5')
 
-prediction = network.predict(model, seed, max_t, n_epochs=3600)
+prediction = network.predict(model, seed, max_t, n_epochs=int(2000/7.5))
 
-file_name = 'sample_song_3600_epochs'
+file_name = 'quick_2'
 
 utils.save_new_song(prediction, file_name)
